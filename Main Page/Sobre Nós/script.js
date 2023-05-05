f51ad79d467da7eb2fdc4fc5f3eafe4bd2da8f67
rochaarafael <pennharafael@gmail.com>
2023-05-05 01:18:33 -0300

    Reorganização de documentos

--- Main Page/Sobre Nós/script.js
+++
@@ -1,9 +0,0 @@
-const cabecalho = document.getElementsByTagName("header")[0]
-const topoNav = cabecalho.offsetTop; //Calcula a distância de px do top deste elemento pro elemento Pai
-
-
-onscroll=function fixarMenuNoTopo() {
-    if (window.pageYOffset >= topoNav) {
-        cabecalho.classList.add("FixoNoTopo");
-    } else cabecalho.classList.remove("FixoNoTopo")
-}
